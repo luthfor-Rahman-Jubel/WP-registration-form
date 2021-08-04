821174d1e33db2b96a159037493b20f43a3fb9ec
Jubel Ahmed <studentsleader44@gmail.com>
2021-08-04 14:36:24 +0600

    WP registration form

---
+++ reg-form.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: Customized Registration From where add some extra features
+Plugin URI: https://github.com/luthfor-rahman-jubel
+Description: A simple Customized WordPress Registration form where add some extra feature like, Phone number, Zip code, Country code and something else.
+Version: 1.0.0
+Author: Jubel Ahmed
+Author URI:
+License: GPLv2 or later
+Text Domain: reg-form
+ */
+
+
+
+ 
